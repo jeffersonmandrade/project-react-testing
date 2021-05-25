027747bd0f730378524e403b2a7eb6d06ccede57
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-05-25 16:34:01 -0300

    test favoritePokemons

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { fireEvent, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import About from '../components/About';
-import renderWithRouter from '../renderWithRouter';
+// import renderWithRouter from '../renderWithRouter';
 
 describe('test component <Abut />', () => {
   test('test h2', () => {
-    const { history } = renderWithRouter(<About />);
+    render(<About />);
     const aboutText = screen.getByRole('heading', {
       level: 2,
       name: /About Pokédex/i,
     });
     expect(aboutText).toBeInTheDocument();
+  });
+  test('image exist', () => {
+    render(<About />);
+    const aboutImage = screen.getByRole('img');
+    expect(aboutImage).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
