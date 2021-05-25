95e8acf94af8dfab815035141f3591028998cba6
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-05-25 17:39:08 -0300

    Requisito 4 feito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('test <FavoritePokemons.js />', () => {
+  test('test mensage Not Found', () => {
+    render(<FavoritePokemons />);
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Test Requisito 4', () => {
+  test('test h2 request', () => {
+    render(<NotFound />);
+    const h2Test = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    const h2Emoticon = screen.getByRole('img', {
+      name: /Crying emoji/,
+    });
+    expect(h2Test).toBeInTheDocument();
+    expect(h2Emoticon).toBeInTheDocument();
+  });
+  test('testa image', () => {
+    render(<NotFound />);
+    const imageTest = screen.getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    expect(imageTest).toHaveAttribute('src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
