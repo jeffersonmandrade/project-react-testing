1468cb2ac6dede8b2e331b1faeb35945a4bae1a9
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-05-25 15:34:23 -0300

    feito o test h2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('test component <Abut />', () => {
+  test('test h2', () => {
+    const { history } = renderWithRouter(<About />);
+    const aboutText = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+    expect(aboutText).toBeInTheDocument();
+  });
+});
