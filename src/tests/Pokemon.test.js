f1c41c0b6ee51104744131f0dc0bd4f83b2836df
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-05-26 07:53:09 -0300

    Testes no Card feitos

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Se é renderizado um card com informações  de determinado pókemon', () => {
+  test('testa o nome,tipo,peso e imagem ', () => {
+    renderWithRouter(<App />);
+    const buttonNormal = screen.getByRole('button', {
+      name: /Normal/i,
+    });
+    fireEvent.click(buttonNormal);
+    // const pokemonName = screen.getByTestId('pokemon-name');
+    const pokemonName = screen.getByText('Snorlax');
+    const pokemonType = screen.getByTestId('pokemon-type');
+    // const pokemonType = screen.getByText(/Normal/i);
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    const pokemonImage = screen.getByRole('img', {
+      name: /Snorlax sprite/i });
+    expect(pokemonName).toBeInTheDocument();
+    expect(pokemonType).toHaveTextContent('Normal');
+    expect(pokemonWeight).toHaveTextContent('Average weight: 460.0 kg');
+    expect(pokemonImage).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/4/40/Spr_5b_143.png');
+  });
+});
